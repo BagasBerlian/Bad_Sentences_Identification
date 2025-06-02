faf39add3f9ad1f7049f2960f1a479c73760928c
BagasBerlian <indonesia4gaming@gmail.com>
2025-06-03 03:12:08 +0700

    change app.py

--- main/app.py
+++ main/app.py
@@ -396,4 +396,5 @@ def get_platform_name(url):
         return 'Unknown'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # app.run(debug=True, host='0.0.0.0', port=5000)
